9358ba7f775c375578b539ecd173b316c27f3268
sandeep1162001 <sandeep11062001@gmail.com>
2024-07-01 07:27:18 +0530

    spell correct

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ db = SQLAlchemy(app)
 app.secret_key = 'Bablu@12345'
 
 
-# Create Database with the name of "User" for Orgnization or School
+# Create Database with the name of "User" 
 class User(db.Model): 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
